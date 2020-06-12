2a3b78984fa4425ead5a1c74194ca99d10a996d9
tc.hung <hungtc@solab.me.ntu.edu.tw>
2020-06-12 13:31:18 +0800

    calculate linear speed of y-axis

--- src/CLaserOdometry2DNode.cpp
+++ src/CLaserOdometry2DNode.cpp
@@ -244,8 +244,9 @@ void CLaserOdometry2DNode::publish()
   odom.pose.pose.orientation = tf::createQuaternionMsgFromYaw(rf2o::getYaw(robot_pose_.rotation()));
   //set the velocity
   odom.child_frame_id = base_frame_id;
-  odom.twist.twist.linear.x = lin_speed;    //linear speed
-  odom.twist.twist.linear.y = 0.0;
+  odom.twist.twist.linear.x = lin_speed_x;    //linear speed
+  //odom.twist.twist.linear.y = 0.0;
+  odom.twist.twist.linear.y = lin_speed_y;
   odom.twist.twist.angular.z = ang_speed;   //angular speed
   //publish the message
   odom_pub.publish(odom);
